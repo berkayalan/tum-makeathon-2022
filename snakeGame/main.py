c000271644d210de45584b0527bfda155212a654
Shivangi0503 <shivangisingh5march@gmail.com>
2022-04-23 15:19:01 +0200

    Trying Multiplayer

--- snakeGame/main.py
+++ snakeGame/main.py
@@ -1,4 +1,3 @@
-
 import math
 import random
 import cvzone
@@ -9,16 +8,18 @@ from cvzone.HandTrackingModule import HandDetector
 import speech_recognition as sr
 from pygame import mixer
 import pygame as pg
+from network import Network
 
 cap = cv2.VideoCapture(0)
 cap.set(3, 1280)
 cap.set(4, 720)
 
-detector = HandDetector(detectionCon=0.8, maxHands=1)
+detector = HandDetector(detectionCon=0.8, maxHands=2)
 
 
 class SnakeGameClass:
     def __init__(self, pathFood, obsPath):
+        #self.net = Network()
         self.points = []  # all points of the snake
         self.lengths = []  # distance between each point
         self.currentLength = 0  # total length of the snake
@@ -39,6 +40,7 @@ class SnakeGameClass:
 
         # scores
         self.score = 0
+        self.obsScore = 0
         self.gameOver = False
 
         pg.init()
@@ -56,17 +58,44 @@ class SnakeGameClass:
     def random_obstacle_location(self):
         self.obsPoint = random.randint(100, 1000), random.randint(100, 600)
 
+    def punch(self, imgMain, leftIndex):
+
+        # # # Draw Obstacle
+        # ox, oy = self.obsPoint
+        # imgMain = cvzone.overlayPNG(imgMain, self.imgObs, (ox - self.wObs // 2, oy - self.hObs // 2))
+        # Check if obstacle is caught
+        cx, cy = leftIndex
+        rx, ry = self.obsPoint
+        # catch obstacles
+        if rx - self.wObs // 2 < cx < rx + self.wObs // 2 and ry - self.hObs // 2 < cy < ry + self.hObs // 2:
+            self.random_obstacle_location()
+            self.obsScore += 1
+            print(self.obsScore)
+
+        if self.gameOver:
+            self.obsScore = 0
+
+
+        return imgMain
+
+
     def update(self, imgMain, currentHead):
         events = pygame.event.get()
         for event in events:
             if event.type == pg.USEREVENT + 1:
                 self.random_obstacle_location()
 
+        nl = '\n'
+        scores = [self.score, self.obsScore]
+
         if self.gameOver:
-            cvzone.putTextRect(imgMain, "Game Over", [300, 400],
+            cvzone.putTextRect(imgMain, "Game Over", [250, 300],
                                scale=7, thickness=5, offset=20)
-            cvzone.putTextRect(imgMain, f'Your Score: {self.score}', [300, 550],
+            cvzone.putTextRect(imgMain, f"Your Score:{self.score}", [250, 450],
                                scale=7, thickness=5, offset=20)
+            cvzone.putTextRect(imgMain, f"Obstacles caught:{self.obsScore}", [100, 550],
+                               scale=7, thickness=5, offset=20,colorT=(255, 0, 0))
+
         else:
             px, py = self.previousHead
             cx, cy = currentHead
@@ -89,17 +118,18 @@ class SnakeGameClass:
             # Check if snake ate the Food
             rx, ry = self.foodPoint
             if rx - self.wFood // 2 < cx < rx + self.wFood // 2 and ry - self.hFood // 2 < cy < ry + self.hFood // 2:
+                mixer.music.load("./data/Pop-sound.mp3")
+
+                # Setting the volume
+                mixer.music.set_volume(0.7)
+
+                # Start playing the song
+                mixer.music.play()
                 self.random_food_location()
 
                 self.allowedLength += 50
                 self.score += 1
                 print(self.score)
-
-            ox, oy = self.obsPoint
-            if ox - self.wObs // 2 < cx < ox + self.wObs // 2 and oy - self.hObs // 2 < cy < oy + self.hObs // 2:
-                print("we are in the if")
-                # self.random_obstacle_location()
-                self.gameOver = True
 
             # Draw Snake
             if self.points:
@@ -112,10 +142,16 @@ class SnakeGameClass:
             imgMain = cvzone.overlayPNG(imgMain, self.imgFood,
                                         (rx - self.wFood // 2, ry - self.hFood // 2))
 
+            # Draw food score
             cvzone.putTextRect(imgMain, f'Score: {self.score}', [50, 80],
                                scale=3, thickness=3, offset=10)
 
+            # Draw obstacle score
+            cvzone.putTextRect(imgMain, f'ObstacleScore: {self.obsScore}', [800, 80], colorR=(255, 0, 0), scale=3,
+                               thickness=3, offset=10)
+
             # Draw Obstacle
+            ox, oy = self.obsPoint
             imgMain = cvzone.overlayPNG(imgMain, self.imgObs, (ox - self.wObs // 2, oy - self.hObs // 2))
 
             # Check for Collision
@@ -123,8 +159,9 @@ class SnakeGameClass:
             pts = pts.reshape((-1, 1, 2))
             cv2.polylines(imgMain, [pts], False, (0, 255, 0), 3)
             minDist = cv2.pointPolygonTest(pts, (cx, cy), True)
+            print(minDist)
 
-            if -1 <= minDist <= 1:
+            if -0.5 <= minDist <= 0.5:
                 print("Hit")
                 self.gameOver = True
                 self.points = []  # all points of the snake
@@ -134,7 +171,24 @@ class SnakeGameClass:
                 self.previousHead = 0, 0  # previous head point
                 self.random_food_location()
 
+            # game over if we hit obstacle
+
+            if ox - self.wObs // 2 < cx < ox + self.wObs // 2 and oy - self.hObs // 2 < cy < oy + self.hObs // 2:
+                print("we are in the if")
+                self.obsScore = 0
+                # self.random_obstacle_location()
+                self.gameOver = True
+
         return imgMain
+
+    # def send_data(self):
+    #     """
+    #     Send position to server
+    #     :return: None
+    #     """
+    #     data = str(self.net.id) + ":" + str(self.player.x) + "," + str(self.player.y)
+    #     reply = self.net.send(data)
+    #     return reply
 
 
 def recognize_speech_from_mic(recognizer, microphone):
@@ -188,6 +242,7 @@ game = SnakeGameClass("./data/coffee.png", "./data/Donut.png") # arg1 = food ima
 
 if __name__ == "__main__":
 
+
     # Voice recognition
     # Starting the mixer
     mixer.init()
@@ -221,6 +276,7 @@ if __name__ == "__main__":
     NUM_GUESSES = 3
     PROMPT_LIMIT = 5
 
+
     if guess_is_correct:
         guess_is_correct = False
         run = True
@@ -229,10 +285,25 @@ if __name__ == "__main__":
             img = cv2.flip(img, 1)
             hands, img = detector.findHands(img, flipType=False)
 
-            if hands:
-                lmList = hands[0]['lmList']
-                pointIndex = lmList[8][0:2]
-                img = game.update(img, pointIndex)
+            for hand in hands:
+                if hand['type'] == 'Left':
+                    lmList = hand['lmList']
+                    leftIndex = lmList[8][0:2]
+                    img = game.punch(img,hand['center'])
+                    print (" yes left")
+                if hand['type'] == 'Right':
+                    lmList = hand['lmList']
+                    pointIndex = lmList[8][0:2]
+                    img = game.update(img, pointIndex)
+
+
+
+            # if hands:
+            #     lmList = hands[0]['lmList']
+            #     pointIndex = lmList[8][0:2]
+            #     img = game.update(img, pointIndex)
+
+
             cv2.imshow("Image", img)
             key = cv2.waitKey(1)
 
